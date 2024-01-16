54633412912e44a65809c251bd19606aa4a2f56c
rodrigo <rd.silva@ubi.pt>
2024-01-16 20:44:08 +0000

    following the freecodecamp tutorial

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,17 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import ArtistsList from "./views/ArtistsList";
-import CreateArtist from "./components/Artist/CreateArtist";
-
-import { Link } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
+import { Login, Signup } from "./pages";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <Link to="/" className="navbar-brand">MERN Stack CRUD</Link>
-          <div className="collpase navbar-collapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="navbar-item">
-                <Link to="/" className="nav-link">Artists</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/add" className="nav-link">Create Artist</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <br/>
-        <Routes>
-          <Route path="/" element={<ArtistsList />} />
-          <Route path="/add" element={<CreateArtist />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
   );
 }
-
 
 export default App;
